713c2ccb9017706e726617ead63dfd5d305d2741
Joy Xie <109412084+joyxie1018@users.noreply.github.com>
2025-10-28 00:12:38 +0800

    [amend] rwd

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,34 +16,7 @@
       </div>
 
       <v-row>
-        <!-- <v-col cols="12">
-          <v-card
-            class="py-4"
-            color="surface-variant"
-            image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
-            prepend-icon="mdi-rocket-launch-outline"
-            rounded="lg"
-            variant="tonal"
-          >
-            <template #image>
-              <v-img position="top right" />
-            </template>
-
-            <template #title>
-              <h2 class="text-h5 font-weight-bold">
-                Get started
-              </h2>
-            </template>
-
-            <template #subtitle>
-              <div class="text-subtitle-1">
-                Change this page by updating <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in <v-kbd>components/HelloWorld.vue</v-kbd>.
-              </div>
-            </template>
-          </v-card>
-        </v-col> -->
-
-        <v-col v-for="link in links" :key="link.href" cols="6">
+        <v-col v-for="link in links" :key="link.href" cols="12" md="6">
           <v-card
             append-icon="mdi-open-in-new"
             class="py-4"
